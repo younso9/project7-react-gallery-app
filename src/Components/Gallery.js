1d419790c89d2236c63945ba1f132d9896c111bb
younso9 <sonya.youngblood@verizon.com>
2019-08-19 13:24:05 -0400

    Update Gallery.js

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 //import the variable apiKey from config.js
-import apiKey from './config';
+import apiKey from "./config";
 
 //import our Components to be displayed in this render
-import Photo from './Photo';
-import NotFound from './NotFound';
-import Loading from './ImagesLoading';
+import Photo from "./Photo";
+import NotFound from "./NotFound";
+import Loading from "./ImagesLoading";
 
 class Gallery extends Component {
-  constructor () {
+  constructor() {
     //execute default constructor
     super();
 
@@ -19,39 +19,38 @@ class Gallery extends Component {
     };
   }
 
-  fetchPhotos = (query) => {
+  fetchPhotos = query => {
     //when loading the page, empty the state variables
     //so the render will show default state while the images load
     this.setState({ photos: [], isLoading: true });
 
     //same but include specification in extras parameter to request url_o, url_z, url_c - different sizes for the images (when available per image)
-    //construct uri for Flickr - embed the API key, tags equal to query term, 24 items per page, 1st page of content, JSON format
-    //const uri = "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=yourkey&tags=searchterm&extras=url_o,url_z,url_c&per_page=24&page=1&format=json&nojsoncallback=1".replace("yourkey", apiKey).replace("searchterm", query);
 
-    //same but embed the variables in the string using ${}
     const uri = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&extras=url_o,url_z,url_c&per_page=24&page=1&format=json&nojsoncallback=1`;
 
     //HTTP GET the URI, convert the response data to JSON, assign the photos state variable and set state isLoading to false, signifying the photos are loaded
     fetch(uri)
-    .then(response => response.json())
-    .then(responseData => {
-      this.setState({ photos: responseData.photos.photo, isLoading: false });
-    })
-    .catch(error => {
-      console.log('Error fetching and parsing data', error);
-    });
-  }
+      .then(response => response.json())
+      .then(responseData => {
+        this.setState({ photos: responseData.photos.photo, isLoading: false });
+      })
+      .catch(error => {
+        console.log("Error fetching and parsing data", error);
+      });
+  };
 
   //https://stackoverflow.com/questions/28329382/understanding-unique-keys-for-array-children-in-react-js
   //https://www.flickr.com/services/api/misc.urls.html
 
   //construct the photo URI for this particular JSON photo item: use the flickr format, display using the Photo Component
   mapJsonToPhotoComponents = (photo, i) => {
-    let uri = '';
+    let uri = "";
 
-      uri = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`;
-    return <Photo src={uri} key={i}/>;
-  }
+    uri = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${
+      photo.id
+    }_${photo.secret}.jpg`;
+    return <Photo src={uri} key={i} />;
+  };
 
   //when the Gallery component mounts (fully loads), run fetchPhotos to get the photos for params 'type'
   //type being the text in /gallery/:type
@@ -64,35 +63,36 @@ class Gallery extends Component {
     if (this.props.location.key !== prevProps.location.key) {
       this.fetchPhotos(this.props.match.params.type);
     }
-}
+  }
 
- render() {
-   let images = [];
-   let content ='';
+  render() {
+    let images = [];
+    let content = "";
 
-   //if the photos aren't loading, display the name
-   if (!this.state.isLoading)
-   {
-    content = "Displaying " + this.props.match.params.type;
-   }
-   //if the photos array has content display them
-    if (this.state.photos.length > 0)
-    {
+    //if the photos aren't loading, display the name
+    if (!this.state.isLoading) {
+      content = "Displaying " + this.props.match.params.type;
+    }
+    //if the photos array has content display them
+    if (this.state.photos.length > 0) {
       images = this.state.photos.map(this.mapJsonToPhotoComponents);
     }
     //if there are no photos in the array, and the isLoading is false, then this must be an empty search: load NotFound component
-      else if (!this.state.isLoading)
-      {
-      images = <NotFound/>;
-      }
-      //otherwise, display the generic Loading panel
-      else
-      {
-        images = <Loading/>;
-      }
-      //render the photo-container with the content and images variables within
-    return <div className="photo-container"><h2>{content}</h2><ul>{images}</ul></div>;
- }
+    else if (!this.state.isLoading) {
+      images = <NotFound />;
+    }
+    //otherwise, display the generic Loading panel
+    else {
+      images = <Loading />;
+    }
+    //render the photo-container with the content and images variables within
+    return (
+      <div className="photo-container">
+        <h2>{content}</h2>
+        <ul>{images}</ul>
+      </div>
+    );
+  }
 }
 
 export default Gallery;
